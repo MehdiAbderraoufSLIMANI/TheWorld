73b0d8ade0dc92852f2b80cd453c9d8adfc901bd
justmahdii <didoslimani10@gmail.com>
2022-12-23 18:39:38 +0100

    adding http package and learing how async work

--- theworld/lib/pages/Location.dart
+++ theworld/lib/pages/Location.dart
@@ -8,6 +8,32 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
+  void getData() async {
+    //! semulation of geteing data
+    int a = 0;
+    print("wait...");
+    await Future.delayed(const Duration(seconds: 3), () {
+      a = 1;
+      print("mahdi.");
+    });
+
+    await Future.delayed(const Duration(seconds: 2), () {
+      if (a == 1) {
+        print("21.");
+      } else {
+        print("errour : a = $a");
+      }
+    });
+
+    print("done");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
